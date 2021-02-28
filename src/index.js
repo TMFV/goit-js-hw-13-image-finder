d8027e6692ea642f09cc766375240aadd8082921
TMFV <68297974+TMFV@users.noreply.github.com>
2021-02-28 22:34:44 +0200

    build1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './styles.css';
 import searchImgFn from './js/apiService';
 import gallery from './templates/gallery.hbs';
 
-
 const debounce = require('lodash.debounce');
 const searchForm = document.querySelector('#search-form');
 const divMark = document.querySelector('.mark');
@@ -10,8 +9,6 @@ const apiKey = '20461350-36527ad634bc0878b1b72e118';
 const firstPage = 1;
 let currentNextPage = 1;
 let searchWords;
-
-
 
 searchForm.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -21,21 +18,15 @@ searchForm.addEventListener('submit', (event) => {
     const galleryMarkup = gallery(title);
     divMark.insertAdjacentHTML('beforeend', galleryMarkup);
     const btn = document.querySelector('.btn-load-more');
-    
     btn.addEventListener('click', (event) => {
-        loadMoreFn(event, currentNextPage);
-        
+        loadMoreFn(event, currentNextPage);  
     });
-
     createFirstPage(searchWords);
     searchForm.reset();
 });
 
 const createFirstPage = (words) => {
-    
-    searchImgFn(words, firstPage, apiKey);
-    
-}
+    searchImgFn(words, firstPage, apiKey);}
 
 function loadMoreFn(event, page) { 
     console.log(`click`, event);
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,26 +1,17 @@
 export default searchImgFn;
-
 import card from '../templates/card.hbs';
 
 async function searchImgFn(words, page, key) {
     await fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${words}&page=${page}&per_page=12&key=${key}`).then((response) => response)
         .then(data => data.json())
-        .then(arr => {
-            let resultArray;
-            resultArray = arr.hits;
-            console.log(resultArray);
-            console.log(arr.hits);
+            .then(arr => {
+                let resultArray;
+                resultArray = arr.hits;
                 const nextPageMarkup = card(resultArray);
                 const ulRef = document.querySelector('.gallery');
-            ulRef.insertAdjacentHTML('beforeend', nextPageMarkup);
-             
-            console.log(document.documentElement.offsetHeight);
-            
-            
-            
-        })
-   window.scrollTo({
-                top: document.documentElement.offsetHeight,
-                behavior: 'smooth',});
+                ulRef.insertAdjacentHTML('beforeend', nextPageMarkup);})
+        window.scrollTo({
+            top: document.documentElement.offsetHeight,
+            behavior: 'smooth',});
 };
 
